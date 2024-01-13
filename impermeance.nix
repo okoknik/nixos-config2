3574b5629e800f08883b7d15eef2af6076358da2
simstuff <82052330+simstuff@users.noreply.github.com>
2024-01-13 14:31:57 +0100

    Create impermeance.nix

---
+++ impermeance.nix
@@ -0,0 +1,45 @@
+{
+  config,
+  pkgs,
+  impermanence,
+  ...
+}: {
+  imports = [
+    impermanence.nixosModule
+  ];
+
+
+# restore empty subvolume for impermeance
+  boot.initrd.postDeviceCommands = pkgs.lib.mkBefore ''
+    mkdir -p /mnt
+
+    mount -o subvol=/ /dev/sda3 /mnt
+
+    btrfs subvolume list -o /mnt/root |
+    cut -f9 -d' ' |
+    while read subvolume; do
+      echo "deleting /$subvolume subvolume..."
+      btrfs subvolume delete "/mnt/$subvolume"
+    done &&
+    echo "deleting /root subvolume..." &&
+    btrfs subvolume delete /mnt/root
+
+    echo "restoring blank /root subvolume..."
+    btrfs subvolume snapshot /mnt/root-blank /mnt/root
+
+    umount /mnt
+  '';
+
+  # impermeance config
+  environment.persistence."/persist" = {
+    directories = [
+      "/etc/nixos"
+      "/etc/NetworkManager/system-connections"  
+   ];
+    files = [
+      "/etc/machine-id"
+      "/etc/ssh/ssh_host_ed25519_key"
+      "/etc/ssh/ssh_host_ed25519_key.pub"
+      "/etc/ssh/ssh_host_rsa_key"
+      "/etc/ssh/ssh_host_rsa_key.pub"
+    ];
