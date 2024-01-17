6b23b5a08ea148ab76d877dcb858d9f0383e3d87
Niklas Oetken <oetkenniklas4@gmail.com>
2024-01-17 20:30:12 +0100

    add mask option boot mount

---
+++ install-scripts/btrfs_install.sh
@@ -0,0 +1,44 @@
+DISK=/dev/sda
+
+#partition disk
+sudo parted "$DISK" -- mklabel gpt
+sudo parted "$DISK" -- mkpart ESP fat32 1MB 512MB
+sudo parted "$DISK" -- set 1 esp on
+sudo parted "$DISK" -- mkpart Swap linux-swap 512MiB 4GiB
+sudo mkswap -L SWAP "$DISK"2
+sudo swapon "$DISK"2
+sudo parted "$DISK" -- mkpart primary 4GiB 100%
+sudo mkfs.fat -F 32 -n BOOT  "$DISK"1
+
+sudo mkfs.btrfs -f -L Butter "$DISK"3
+sudo mount -t btrfs "$DISK"3 /mnt
+sudo btrfs subvolume create /mnt/root
+sudo btrfs subvolume create /mnt/home
+sudo btrfs subvolume create /mnt/nix
+sudo btrfs subvolume create /mnt/persist
+sudo btrfs subvolume create /mnt/log
+
+sudo btrfs subvolume snapshot -r /mnt/root /mnt/root-blank
+sudo umount /mnt
+
+
+sudo mount -o subvol=root,compress=zstd,noatime "$DISK"3 /mnt
+
+sudo mkdir /mnt/home
+sudo mount -o subvol=home,compress=zstd,noatime "$DISK"3 /mnt/home
+
+sudo mkdir /mnt/nix
+sudo mount -o subvol=nix,compress=zstd,noatime "$DISK"3 /mnt/nix
+
+sudo mkdir /mnt/persist
+sudo mount -o subvol=persist,compress=zstd,noatime "$DISK"3 /mnt/persist
+
+sudo mkdir -p /mnt/var/log
+sudo mount -o subvol=log,compress=zstd,noatime "$DISK"3 /mnt/var/log
+
+sudo mkdir /mnt/boot
+sudo mount "$DISK"1 /mnt/boot -o umask=0077
+
+sudo nixos-generate-config --root /mnt
+
+sudo nano /mnt/etc/nixos/configuration.nix
