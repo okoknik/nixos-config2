78c87d19da8e1edab3b4f488c97d5c9074bdc258
Niklas Oetken <oetkenniklas4@gmail.com>
2024-04-04 15:29:40 +0200

    clean up default.nix#

--- flake.nix
+++ flake.nix
@@ -94,7 +94,7 @@
 		      home-manager.nixosModules.home-manager
           {
             home-manager.extraSpecialArgs = {
-              inherit username;
+              #inherit username;
             };
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
