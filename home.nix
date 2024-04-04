87e229cfb1784ee8beb278c2c4e8a0a3da776819
Niklas Oetken <oetkenniklas4@gmail.com>
2024-04-04 18:20:14 +0200

    clean up default.nix#

--- home.nix
+++ home.nix
@@ -86,7 +86,6 @@
       env.TERM = "xterm-256color";
       font = {
         size = 12;
-        draw_bold_text_with_bright_colors = true;
       };
       startup_mode = "Maximized";
       scrolling.multiplier = 5;
