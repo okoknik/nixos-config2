d30cbbaeb35fa3c78fe361a9066ba925ef27ce36
Niklas Oetken <oetkenniklas4@gmail.com>
2024-04-04 16:29:24 +0200

    clean up default.nix#

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
 
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
+      url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     impermanence.url = "github:nix-community/impermanence";
