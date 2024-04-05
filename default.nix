b886eaec743abd031a3650b2add93a1809fa2d4c
simstuff <oetkenniklas@gmail.com>
2024-04-05 15:23:08 +0200

    add autoup

--- default.nix
+++ default.nix
@@ -172,7 +172,7 @@ hardware.nvidia = {
 			enableOffloadCmd = true;
 		};
 		# Make sure to use the correct Bus ID values for your system!
-		intelBusId = "PCI:00:02.0";
+		intelBusId = "PCI:00:02:0";
 		nvidiaBusId = "PCI:14:0:0";
 	};
   };
