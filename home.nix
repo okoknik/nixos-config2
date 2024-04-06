6e19460cc817400783763c092ffd1dc478a067bb
simstuff <oetkenniklas@gmail.com>
2024-04-06 14:12:38 +0200

    nix optimization

--- home.nix
+++ home.nix
@@ -92,7 +92,7 @@
   pkgs.vimPlugins.nvim-tree-lua
   {
     plugin = pkgs.vimPlugins.vim-startify;
-    config = "let g:startify_change_to_vcs_root = 0";
+    #config = "let g:startify_change_to_vcs_root = 0";
   }
   ];
  };
