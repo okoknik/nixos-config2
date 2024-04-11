c1e89fa1d8cc35d6c5d3f818b4e1997e6e192542
simstuff <oetkenniklas@gmail.com>
2024-04-11 12:48:43 +0200

    add kdeconnect

--- configuration.nix
+++ configuration.nix
@@ -281,8 +281,8 @@
 # services.openssh.enable = true;
 
 # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [ 8384 22000 ];
-  networking.firewall.allowedUDPPorts = [ 22000 21027 ];
+  networking.firewall.allowedTCPPorts = [ 8384 22000 1714 ];
+  networking.firewall.allowedUDPPorts = [ 22000 21027 1714 ];
 # Or disable the firewall altogether.
 # networking.firewall.enable = false;
 
