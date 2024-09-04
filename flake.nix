0cb9c92b41f985fcbdafc2b0d2de81e8b02f123c
okoknik <oetkenniklas@gmail.com>
2024-09-04 19:47:43 +0200

    remove nixvim

--- flake.nix
+++ flake.nix
@@ -26,7 +26,7 @@
   # can be referenced by their names. 
   # The `@` syntax here is used to alias the attribute set of the
   # inputs's parameter, making it convenient to use inside the function.
-  outputs = { self, nixpkgs, home-manager, impermanence, nixvim, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, impermanence, ... }@inputs: { # insert nixvim here
     nixosConfigurations = {
       "framework" = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
