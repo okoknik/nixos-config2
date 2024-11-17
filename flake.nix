a0b078ab6eca48d203ab0328f29c09a3c152abf8
okoknik <oetkenniklas@gmail.com>
2024-11-17 20:52:13 +0100

    add zellij

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     # The most widely used is `github:owner/name/reference`,
     # which represents the GitHub repository URL + branch/commit-id/tag.
     
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     home-manager = {
       url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
